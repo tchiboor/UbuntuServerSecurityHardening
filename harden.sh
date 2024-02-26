41a39ab0a79e2909f1b9e80fbbeb917cdd74272c
Trevor Henry Chiboor <chiboorat@gmail.com>
2024-02-26 10:38:25 +0200

    Updated code to use functions

--- harden.sh
+++ harden.sh
@@ -7,32 +7,63 @@ YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
+# Function to install dependencies
+install_dependencies() {
+    echo -e "${BLUE}Installing dependencies${NC}"
+    sleep 3
+    apt install figlet -y
+    echo
+    echo -e "${BLUE}Installing dependencies: ${GREEN}DONE${NC}"
+    echo
+}
 
-# CONFIGURE THE TIME ZONE
-# Prompt the user to select a timezone
-echo -e "${BLUE}Please select your timezone:${NC}"
-sleep 5
-dpkg-reconfigure tzdata
+# Function to create report file
+create_report_file() {
+    echo -e "${BLUE}Creating text file for the report${NC}"
+    touch Report.txt
+    echo
+    echo -e "${BLUE}Reporting file created: ${GREEN}DONE${NC}"
+    echo
+}
 
-# Optional: Print out the current date/time to verify the setting
-date
-echo
-echo
+# Function to configure timezone
+configure_timezone() {
+    echo -e "${BLUE}Configuring timezone${NC}"
+    echo -e "${BLUE}Please select your timezone:${NC}"
+    sleep 3
+    dpkg-reconfigure tzdata
+    date
+    echo -e "${BLUE}Configure timezone: ${GREEN}DONE${NC}"
+    echo -e "${BLUE}Configure timezone: ${GREEN}DONE${NC}" >> Report.txt
+    echo
+}
 
-# UPDATE THE SYSTEM
-echo -e "${BLUE}Updating the package lists and upgrading the installed packages${NC}"
+# Function to update the system
+update_system() {
+    echo -e "${BLUE}Updating the package lists and upgrading the installed packages${NC}"
+    sleep 3
+    apt update
+    apt upgrade -y
+    echo -e "${BLUE}System update and upgrade: ${GREEN}DONE${NC}"
+    echo -e "${BLUE}System update and upgrade: ${GREEN}DONE${NC}" >> Report.txt
+}
 
-sleep 5
-apt update
-apt upgrade -y
+# Main function to run all tasks
+main() {
+    # Install dependencies
+    install_dependencies
 
-# ENSURE ONLY ROOT HAS UID of 0
-# SHH
-# IP TABLE
-# PORT SECURITY
-# FAIL2BAN
-# ROOTKITHUNTER
-# DISABLE COMPILERS
-# FILE PERMISSIONS
-# SECURE SHARED MEMORY
-# DISABLE ROOT LOGIN
+    # Create report file
+    create_report_file
+
+    # Configure timezone
+    configure_timezone
+
+    # Update the system
+    update_system
+
+    # Add more tasks/functions as needed
+}
+
+# Run the main function
+main
